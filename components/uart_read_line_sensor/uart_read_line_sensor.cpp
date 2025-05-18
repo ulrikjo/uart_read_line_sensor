97adcd279afa97c839a2cccbfc91e55067977419
ulrikjo <53315889+ulrikjo@users.noreply.github.com>
2025-05-18 15:17:48 +0200

    Update uart_read_line_sensor.cpp

--- components/uart_read_line_sensor/uart_read_line_sensor.cpp
+++ components/uart_read_line_sensor/uart_read_line_sensor.cpp
@@ -1,7 +1,9 @@
 #include "uart_read_line_sensor.h"
 
+namespace esphome {
 namespace uart_read_line_sensor {
 
-// Nothing needed here for now
+// Nothing additional needed in the .cpp for now
 
-}
+}  // namespace uart_read_line_sensor
+}  // namespace esphome
